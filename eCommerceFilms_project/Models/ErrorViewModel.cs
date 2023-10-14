e1f657a5f90523f28caca83da5cd5d2bbca0d772
SergiyBo1ko <boiko0624@gmail.com>
2023-10-14 15:29:06 -0400

    Add project files.

---
+++ eCommerceFilms_project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommerceFilms_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
